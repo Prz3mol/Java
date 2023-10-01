42a1d5ac15fc5dddda58ee64e63e6a71c8e8e19b
Prz3mol <121061134+Prz3mol@users.noreply.github.com>
2023-10-02 00:03:07 +0200

    Update Kalkulator.java

--- Kalkulator.java
+++ Kalkulator.java
@@ -1,41 +1,27 @@
-package com.example.kalkulator;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 import java.util.Scanner;
-
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void main(String[] args) {
+public class kalkulator {
+	
+	public static void main(String[] args) {
         System.out.println("Hello World");
+		Scanner obj1 = new Scanner(System.in);
+        
+		System.out.println("Podaj pierwszÄ… liczbÄ™:");
 
-        System.out.println("Podaj pierwszą liczbę:");
-        Scanner obj1 = new Scanner(System.in);
         int x = obj1.nextInt();
-        System.out.println("Podaj drugą liczbe:");
-        Scanner obj2 = new Scanner(System.in);
-        int y = obj2.nextInt();
-        System.out.println("Jaką operacje chcesz wykonac" +
+        System.out.println("Podaj drugÄ… liczbe:");
+		int y = obj1.nextInt();
+		
+		
+		
+        System.out.println("JakÄ… operacje chcesz wykonac" +
                 "1+ " +
                 "2- " +
                 "3* " +
                 "4/ " +
                 "5% ");
-        Scanner operacja = new Scanner(System.in);
-        int op = operacja.nextInt();
-        switch (op) {
+        int op = obj1.nextInt();
+        	
+		switch (op) {
             case 1:
                 System.out.println(x + y);
                 break;
@@ -52,5 +38,5 @@ public class HelloApplication extends Application {
                 System.out.println(x % y);
                 break;
         }
-    }
+	}
 }
