538ad1e20722aaa4f401af5766f8da7ee50a0a5e
Prz3mol <121061134+Prz3mol@users.noreply.github.com>
2023-11-22 12:55:38 +0100

    Update bubblesort.java

--- bubblesort.java
+++ bubblesort.java
@@ -1,18 +1,27 @@
 public class bubblesort {
     public static void main (String[] args){
-        int[] numbers = {5, 4, 7, 2, 1, 6, 3};
-        int n = numbers.length;
-        for (int i=0; i<numbers.length - 1; i++){
+
+        int[] numbers =new int[10];
+        for (int p=0; p<numbers.length;p++)
+            numbers[p] = (int)(Math.random()*100);
+        boolean swap;
+        for (int i=0; i<numbers.length -1; i++){
+            swap=false;
             for (int j = 0; j < numbers.length - i - 1;j++) {
                 if (numbers[j] > numbers[j + 1]) {
                     int temp = numbers[j];
                     numbers[j] = numbers[j + 1];
                     numbers[j + 1] = temp;
+                    swap=true;
                 }
+                }
+                if (swap) {
+                    System.out.print("Po przejściu " + (i + 1) + ": ");
+                    for (int k = 0; k < numbers.length; k++) {
+                        System.out.print(numbers[k] + " ");
+                    }
+                System.out.println();
             }
-        }
-        for (int i=0; i < numbers.length;i++){
-            System.out.print(numbers[i]+ " ");
         }
     }
 }
